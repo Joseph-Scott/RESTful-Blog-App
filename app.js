911bd5b3b50e7d8f9dfa8b61772b180cffafdf63
Joe Scott <jbs198585@gmail.com>
2019-08-26 20:08:52 -0400

    mongoose/model config

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ mongoose       = require("mongoose"),
 express        = require("express"),
 app            = express();
 
+// APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-// title
-// image
-// body
-// created
+// MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTFUL ROUTES
+
 
 
 
