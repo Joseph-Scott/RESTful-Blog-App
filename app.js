0d10611e81622cbe0bd940a5f5b1f7b878ce0458
Joe Scott <jbs198585@gmail.com>
2019-08-29 20:29:42 -0400

    added new route and new.ejs file, fixed some styling issues with 'new' page

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.get("/", function(req, res){
   res.redirect("/blogs");
 })
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
   Blog.find({}, function(err, blogs){
     if(err){
@@ -35,6 +36,12 @@ app.get("/blogs", function(req, res){
   });
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+  res.render("new");
+})
+
+// CREATE ROUTE
 app.listen(3000, function(){
   console.log("SERVER IS RUNNING!");
 });
