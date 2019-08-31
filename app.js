4b4295900e100fe66a304e17f270076f0915094f
Joe Scott <jbs198585@gmail.com>
2019-08-31 08:32:39 -0400

    added create route

--- app.js
+++ app.js
@@ -42,6 +42,18 @@ app.get("/blogs/new", function(req, res){
 })
 
 // CREATE ROUTE
+app.post("/blogs", function(req, res){
+  // create blog
+  Blog.create(req.body.blog, function(err, newBlog){
+    if(err){
+      res.render("new");
+    } else {
+      // then, redirect to the index
+      res.redirect("/blogs");
+    }
+  });
+});
+
 app.listen(3000, function(){
   console.log("SERVER IS RUNNING!");
 });
